b276617ccac510297f82e4349d198bc1de2ce17a
EttouibiaSalma <ettoui.salma@gmail.com>
2022-10-19 13:20:38 +0100

    signup form

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+}
